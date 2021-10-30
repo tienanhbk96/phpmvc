db5f2b4543b74644d8c492a8e756539918c739c9
tienanhbk96 <tienanhbk96@gmail.com>
2021-10-30 16:56:31 +0700

    4_done

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,15 @@
+<?php
+	include '../classes/adminlogin.php'
+?>
+<?php
+	$class = new adminlogin();
+	if($_SERVER ['REQUEST_METHOD'] === 'POST'){
+		$adminUser = $_POST['adminUser'];
+		$adminPassword = $_POST['adminPassword'];
+	}
+
+	$login_check = $class->login_admin($adminUser, $adminPassword);
+?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
@@ -7,13 +19,13 @@
 <body>
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Username" required="" name="adminUser"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Password" required="" name="adminPassword"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
---
+++ classes/adminlogin.php
@@ -0,0 +1,21 @@
+<?php
+    class adminlogin {
+        public function _construct(){
+            
+        }
+
+        public function login_admin(){
+
+        }
+
+        public function admin_check(){
+            
+        }
+
+        public function login_destroy(){
+            
+        }
+    }
+
+
+?>
