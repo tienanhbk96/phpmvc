80449e02253d06fa95e740e91d01290cf7989ea6
tienanhbk96 <tienanhbk96@gmail.com>
2021-11-05 00:09:23 +0700

    8_6_00

--- admin/catlist.php
+++ admin/catlist.php
@@ -1,5 +1,10 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php include '../classes/category.php' ?>
+<?php
+    $cat = new category();
+    
+?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Category List</h2>
@@ -13,44 +18,12 @@
 						</tr>
 					</thead>
 					<tbody>
+						<?php 
+							$show_cat = $cat->show_category();
+						?>
 						<tr class="odd gradeX">
 							<td>01</td>
 							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
 							<td><a href="">Edit</a> || <a href="">Delete</a></td>
 						</tr>
 					</tbody>
---
+++ classes/category.php
@@ -0,0 +1,45 @@
+<?php
+    include '../lib/database.php';
+    include '../helpers/format.php';
+?>
+
+<?php
+    class category {
+        private $db;
+        private $fm;
+
+        public function __construct(){
+            $this->db = new Database();
+            $this->fm = new Format();
+        }
+            
+        public function insert_category($catName){
+            $catName = $this->fm->validation($catName);
+
+            $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+            if(empty($catName)){
+                $alert = "<span class='error'>Category must be not empty</span>";
+                return $alert;
+            }else{
+                $query = "INSERT INTO tbl_category(catName) VALUES('$catName')";
+                $result = $this->db->insert($query);
+                if($result){
+                    $alert = "<span class='success'>Insert Category Successfully</span>";
+                    return $alert;
+                }else{
+                    $alert = "<span class='error'>Insert Category not Success</span>";
+                    return $alert;
+                }
+            }
+        }
+
+        public function show_category(){
+            $query = "SELECT * FROM tbl_category order by catId desc";
+            $result = $this->db->select($query);
+            return $result;
+        }
+    }
+
+
+?>
