e71e5ec8b72e00ba2a3de33bac8fbd756414539c
tienanhbk96 <tienanhbk96@gmail.com>
2021-11-08 17:13:33 +0700

    16_done

--- admin/productadd.php
+++ admin/productadd.php
@@ -1,8 +1,10 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php include_once '../classes/brand.php';?>
+<?php include_once '../classes/category.php';?>
 <div class="grid_10">
     <div class="box round first grid">
-        <h2>Add New Product</h2>
+        <h2>Thêm sản phẩm</h2>
         <div class="block">               
          <form action="" method="post" enctype="multipart/form-data">
             <table class="form">
@@ -12,7 +14,7 @@
                         <label>Name</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Product Name..." class="medium" />
+                        <input type="text" name="productName" placeholder="Enter Product Name..." class="medium" />
                     </td>
                 </tr>
 				<tr>
@@ -20,11 +22,19 @@
                         <label>Category</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
-                            <option>Select Category</option>
-                            <option value="1">Category One</option>
-                            <option value="2">Category Two</option>
-                            <option value="3">Category Three</option>
+                        <select id="select" name="category">
+                            <option>--- Select Category ---</option>
+                            <?php
+                                $cat = new category();
+                                $catlist = $cat->show_category();
+                                if($catlist){
+                                    while($result = $catlist->fetch_assoc()){
+                            ?>
+                                    <option value="<?php echo $result['catId'] ?>"><?php echo $result['catName'] ?></option>
+                            <?php
+                                    }
+                                }
+                            ?>
                         </select>
                     </td>
                 </tr>
@@ -33,11 +43,19 @@
                         <label>Brand</label>
                     </td>
                     <td>
-                        <select id="select" name="select">
-                            <option>Select Brand</option>
-                            <option value="1">Brand One</option>
-                            <option value="2">Brand Two</option>
-                            <option value="3">Brand Three</option>
+                        <select id="select" name="brand">
+                            <option>--- Select Brand ---</option>
+                            <?php
+                                $brand = new brand();
+                                $brandlist = $brand->show_brand();
+                                if($brandlist){
+                                    while($result = $brandlist->fetch_assoc()){
+                            ?>
+                                    <option value="<?php echo $result['brandId'] ?>"><?php echo $result['brandName'] ?></option>
+                            <?php
+                                    }
+                                }
+                            ?>
                         </select>
                     </td>
                 </tr>
@@ -47,7 +65,7 @@
                         <label>Description</label>
                     </td>
                     <td>
-                        <textarea class="tinymce"></textarea>
+                        <textarea name="product_desc" class="tinymce"></textarea>
                     </td>
                 </tr>
 				<tr>
@@ -55,7 +73,7 @@
                         <label>Price</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Enter Price..." class="medium" />
+                        <input type="text" name="price" placeholder="Enter Price..." class="medium" />
                     </td>
                 </tr>
             
@@ -64,19 +82,19 @@
                         <label>Upload Image</label>
                     </td>
                     <td>
-                        <input type="file" />
-                    </td>
+                        <input type="file" name="image" />
+                    </td> 
                 </tr>
 				
 				<tr>
                     <td>
-                        <label>Product Type</label>
+                        <label>Product Type</label> 
                     </td>
                     <td>
-                        <select id="select" name="select">
+                        <select id="select" name="type">
                             <option>Select Type</option>
                             <option value="1">Featured</option>
-                            <option value="2">Non-Featured</option>
+                            <option value="0">Non-Featured</option>
                         </select>
                     </td>
                 </tr>
